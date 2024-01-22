e813fef44f7cb7c6488cfddb13918bba421d06d2
JDavidHA <58662948+JDevArauz@users.noreply.github.com>
2024-01-21 23:50:14 -0600

    MODIFICACION DEL INDEX (HOMEPAGE) Y CREACION DEL LOGIN
    Se modifico el INDEX.HTML para testear el login, realizando el llamado de la libreria de alpine.js para facilitar el manejo de los iconos y transiciones de los mismo en el homepage. El INDEX.JS tiene  el render hacia el login debido a que lo estaba testeando, aun no he conectado nada con el api, es solo para dejar listo el front

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './Components/Login/Login';
 
+
+//IMPORTANDO LA LIBRERIA DE ALPINE PARA UTILIZAR EN EL RENDER DE LOGIN
+window.Alpine = require('alpinejs');
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+//ESTE RENDER REDIRIGE HACIA EL FRONT DEL LOGIN SIN EMBARGO PUEDE USARSE PARA TESTEOS INDIVIDUALES DE CUALQUIER VISTA 
+root.render(  
+<React.StrictMode>
+  <Login /> 
+</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

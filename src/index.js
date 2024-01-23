bd7b4da5ebd316c101df39fd6585884064b5e24b
Patrick Lisby Córdoba <lisby2103@gmail.com>
2024-01-22 21:12:23 -0600

    Categorys

--- src/index.js
+++ src/index.js
@@ -4,17 +4,25 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Register from './Components/Login/Registration';
 
+import App from './App';//Quitar al terminar de editar
+
 
 //IMPORTANDO LA LIBRERIA DE ALPINE PARA UTILIZAR EN EL RENDER DE LOGIN
 window.Alpine = require('alpinejs');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 //ESTE RENDER REDIRIGE HACIA EL FRONT DEL LOGIN SIN EMBARGO PUEDE USARSE PARA TESTEOS INDIVIDUALES DE CUALQUIER VISTA 
+
 root.render(  
 <React.StrictMode>
-  <Register /> 
+  <App /> 
 </React.StrictMode>
 );
+/*
+<React.StrictMode>
+  <Register /> //Aunque deberia ser login por default
+</React.StrictMode>
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
